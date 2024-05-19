eeb96be58b3130cd7b3c66b04444fa075819d7de
gadyintah <jikiorejo1@gmail.com>
2024-05-20 02:14:18 +0800

    added images for the services grid. Modifying the landing page

--- components/features-blocks.tsx
+++ components/features-blocks.tsx
@@ -6,20 +6,19 @@ export default function FeaturesBlocks() {
       <div className="absolute inset-0 top-1/2 md:mt-24 lg:mt-0 bg-gray-900 pointer-events-none" aria-hidden="true"></div>
       <div className="absolute left-0 right-0 bottom-0 m-auto w-px p-px h-20 bg-gray-200 transform translate-y-1/2"></div>
 
-      <div className="relative max-w-6xl mx-auto px-4 sm:px-6">
+      <div className="relative max-w-screen- mx-auto px-4 sm:px-6">
+        
         <div className="py-12 md:py-20">
-
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-            <h2 className="h2 mb-4">Explore the solutions</h2>
+            <h2 className="h2 mb-4">Services Offered</h2>
             <p className="text-xl text-gray-600">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur excepteur sint occaecat cupidatat.</p>
           </div>
 
           {/* Items */}
-          <div className="max-w-sm mx-auto grid gap-6 md:grid-cols-2 lg:grid-cols-3 items-start md:max-w-2xl lg:max-w-none">
-
+          <div className="max-w-sm mx-auto grid gap-6 md:grid-cols-2 lg:grid-cols-5 items-start md:max-w-2xl lg:max-w-none">
             {/* 1st item */}
-            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl">
+            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl h-full" style={{ backgroundImage: "url('/images/teeth-whitening.jpg')", backgroundSize: 'cover', backgroundPosition: 'center',  }}>
               <svg className="w-16 h-16 p-1 -mt-1 mb-2" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
                 <g fill="none" fillRule="evenodd">
                   <rect className="fill-current text-blue-600" width="64" height="64" rx="32" />
@@ -31,12 +30,12 @@ export default function FeaturesBlocks() {
                   </g>
                 </g>
               </svg>
-              <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">Headless CMS</h4>
+              <h4 className="text-xl text-center font-bold leading-snug tracking-tight mb-1">Oral Prophylaxis and Teeth Whitening</h4>
               <p className="text-gray-600 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
 
             {/* 2nd item */}
-            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl">
+            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl h-full" style={{ backgroundImage: "url('/images/header-bg.png')", backgroundSize: 'cover', backgroundPosition: 'center' }}>
               <svg className="w-16 h-16 p-1 -mt-1 mb-2" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
                 <g fill="none" fillRule="evenodd">
                   <rect className="fill-current text-blue-600" width="64" height="64" rx="32" />
@@ -47,12 +46,13 @@ export default function FeaturesBlocks() {
                   </g>
                 </g>
               </svg>
-              <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">Headless CMS</h4>
+              <h4 className="text-xl text-center font-bold leading-snug tracking-tight mb-1">Dental Check-Up and Ortho Consultation</h4>
               <p className="text-gray-600 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
 
+
             {/* 3rd item */}
-            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl">
+            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl h-full" style={{ backgroundImage: "url('/images/header-bg.png')", backgroundSize: 'cover', backgroundPosition: 'center' }}>
               <svg className="w-16 h-16 p-1 -mt-1 mb-2" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
                 <g fill="none" fillRule="evenodd">
                   <rect className="fill-current text-blue-600" width="64" height="64" rx="32" />
@@ -64,12 +64,12 @@ export default function FeaturesBlocks() {
                   </g>
                 </g>
               </svg>
-              <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">Headless CMS</h4>
+              <h4 className="text-xl text-center font-bold leading-snug tracking-tight mb-1">Tooth Extraction and Odontectomy</h4>
               <p className="text-gray-600 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
 
             {/* 4th item */}
-            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl">
+            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl h-full" style={{ backgroundImage: "url('/images/header-bg.png')", backgroundSize: 'cover', backgroundPosition: 'center' }}>
               <svg className="w-16 h-16 p-1 -mt-1 mb-2" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
                 <g fill="none" fillRule="evenodd">
                   <rect className="fill-current text-blue-600" width="64" height="64" rx="32" />
@@ -81,12 +81,12 @@ export default function FeaturesBlocks() {
                   </g>
                 </g>
               </svg>
-              <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">Headless CMS</h4>
+              <h4 className="text-xl text-center font-bold leading-snug tracking-tight mb-1">Frenectomy and Operculectomy</h4>
               <p className="text-gray-600 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
 
             {/* 5th item */}
-            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl">
+            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl h-full" style={{ backgroundImage: "url('/images/header-bg.png')", backgroundSize: 'cover', backgroundPosition: 'center' }}>
               <svg className="w-16 h-16 p-1 -mt-1 mb-2" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
                 <g fill="none" fillRule="evenodd">
                   <rect className="fill-current text-blue-600" width="64" height="64" rx="32" />
@@ -97,12 +97,12 @@ export default function FeaturesBlocks() {
                   </g>
                 </g>
               </svg>
-              <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">Headless CMS</h4>
+              <h4 className="text-xl text-center font-bold leading-snug tracking-tight mb-1">Tooth Fillings and Cosmetic Dentistry</h4>
               <p className="text-gray-600 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
 
             {/* 6th item */}
-            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl">
+            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl h-full" style={{ backgroundImage: "url('/images/header-bg.png')", backgroundSize: 'cover', backgroundPosition: 'center' }}>
               <svg className="w-16 h-16 p-1 -mt-1 mb-2" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
                 <g fill="none" fillRule="evenodd">
                   <rect className="fill-current text-blue-600" width="64" height="64" rx="32" />
@@ -112,7 +112,67 @@ export default function FeaturesBlocks() {
                   </g>
                 </g>
               </svg>
-              <h4 className="text-xl font-bold leading-snug tracking-tight mb-1">Headless CMS</h4>
+              <h4 className="text-xl text-center font-bold leading-snug tracking-tight mb-1">Dentures and Prosthodontic Rehabilitation</h4>
+              <p className="text-gray-600 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+
+            {/* 7th item */}
+            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl h-full" style={{ backgroundImage: "url('/images/header-bg.png')", backgroundSize: 'cover', backgroundPosition: 'center' }}>
+              <svg className="w-16 h-16 p-1 -mt-1 mb-2" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+                <g fill="none" fillRule="evenodd">
+                  <rect className="fill-current text-blue-600" width="64" height="64" rx="32" />
+                  <g strokeWidth="2" strokeLinecap="square">
+                    <path className="stroke-current text-white" d="M29.714 40.358l-4.777 2.51 1.349-7.865-5.715-5.57 7.898-1.147L32 21.13l3.531 7.155 7.898 1.147L40 32.775" />
+                    <path className="stroke-current text-blue-300" d="M44.571 43.429H34.286M44.571 37.714H34.286" />
+                  </g>
+                </g>
+              </svg>
+              <h4 className="text-xl text-center font-bold leading-snug tracking-tight mb-1">Root Canal Treatment</h4>
+              <p className="text-gray-600 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+
+            {/* 8th item */}
+            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl h-full" style={{ backgroundImage: "url('/images/header-bg.png')", backgroundSize: 'cover', backgroundPosition: 'center' }}>
+              <svg className="w-16 h-16 p-1 -mt-1 mb-2" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+                <g fill="none" fillRule="evenodd">
+                  <rect className="fill-current text-blue-600" width="64" height="64" rx="32" />
+                  <g strokeWidth="2" strokeLinecap="square">
+                    <path className="stroke-current text-white" d="M29.714 40.358l-4.777 2.51 1.349-7.865-5.715-5.57 7.898-1.147L32 21.13l3.531 7.155 7.898 1.147L40 32.775" />
+                    <path className="stroke-current text-blue-300" d="M44.571 43.429H34.286M44.571 37.714H34.286" />
+                  </g>
+                </g>
+              </svg>
+              <h4 className="text-xl text-center font-bold leading-snug tracking-tight mb-1">TADS and Orthodontic Treatment</h4>
+              <p className="text-gray-600 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+
+            {/* 9th item */}
+            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl h-full" style={{ backgroundImage: "url('/images/header-bg.png')", backgroundSize: 'cover', backgroundPosition: 'center' }}>
+              <svg className="w-16 h-16 p-1 -mt-1 mb-2" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+                <g fill="none" fillRule="evenodd">
+                  <rect className="fill-current text-blue-600" width="64" height="64" rx="32" />
+                  <g strokeWidth="2" strokeLinecap="square">
+                    <path className="stroke-current text-white" d="M29.714 40.358l-4.777 2.51 1.349-7.865-5.715-5.57 7.898-1.147L32 21.13l3.531 7.155 7.898 1.147L40 32.775" />
+                    <path className="stroke-current text-blue-300" d="M44.571 43.429H34.286M44.571 37.714H34.286" />
+                  </g>
+                </g>
+              </svg>
+              <h4 className="text-xl text-center font-bold leading-snug tracking-tight mb-1">Bone Graft and Implantology</h4>
+              <p className="text-gray-600 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+
+            {/* 10th item */}
+            <div className="relative flex flex-col items-center p-6 bg-white rounded shadow-xl h-full" style={{ backgroundImage: "url('/images/header-bg.png')", backgroundSize: 'cover', backgroundPosition: 'center' }}>
+              <svg className="w-16 h-16 p-1 -mt-1 mb-2" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
+                <g fill="none" fillRule="evenodd">
+                  <rect className="fill-current text-blue-600" width="64" height="64" rx="32" />
+                  <g strokeWidth="2" strokeLinecap="square">
+                    <path className="stroke-current text-white" d="M29.714 40.358l-4.777 2.51 1.349-7.865-5.715-5.57 7.898-1.147L32 21.13l3.531 7.155 7.898 1.147L40 32.775" />
+                    <path className="stroke-current text-blue-300" d="M44.571 43.429H34.286M44.571 37.714H34.286" />
+                  </g>
+                </g>
+              </svg>
+              <h4 className="text-xl text-center font-bold leading-snug tracking-tight mb-1">Dental Xray</h4>
               <p className="text-gray-600 text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
 
