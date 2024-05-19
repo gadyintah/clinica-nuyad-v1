a49f0ebfc027dadd852b600b629e9c9b57a96e04
gadyintah <jikiorejo1@gmail.com>
2024-05-20 06:18:17 +0800

    Removed extra name at title tab

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -29,6 +29,8 @@ export default function DefaultLayout({
         {children}
 
       </main>
+
+      {/* <Footer /> */}
     </>
   )
 }
--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
-  title: 'Home - Simple',
-  description: 'Page description',
+  title: 'Home',
+  description: 'Landing Page',
 }
 
 import Hero from '@/components/hero'
